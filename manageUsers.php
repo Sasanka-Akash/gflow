083bc6a6a9cd27748d753c205489eee9f7b31b86
SESasanka <157799189+SESasanka@users.noreply.github.com>
2024-07-29 14:28:38 +0530

    main

--- manageUsers.php
+++ manageUsers.php
@@ -14,7 +14,7 @@ if (isset($_SESSION["au"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Products Managment | Gflow</title>
         <link rel="stylesheet" href="style.css">
-        <link rel="icon" href="image/logo.png">
+        <link rel="icon" href="img/g1.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
