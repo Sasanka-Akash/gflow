4194d9adabd2d803c56cb9378881341f03de5862
SESasanka <157799189+SESasanka@users.noreply.github.com>
2024-08-01 02:31:05 +0530

    main

--- adminHeader.php
+++ adminHeader.php
@@ -395,8 +395,8 @@
             </div>
         </div>
 
-        <!-- <script src="script.js"></script> -->
-        <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
+        <script src="script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </body>
 
