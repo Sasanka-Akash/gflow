741f3f286553b5719d30b28160773b8e581b4391
SESasanka <157799189+SESasanka@users.noreply.github.com>
2024-08-02 13:06:28 +0530

    main

--- admindashboard.php
+++ admindashboard.php
@@ -21,6 +21,8 @@ if (isset($_SESSION["au"])) {
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
         <link rel="icon" href="img/g1.png">
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
